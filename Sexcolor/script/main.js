d263532531bd584d8e1ed489fba3abd1c7be47b5
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-04 17:11:55 +0100

    inizio

--- Sexcolor/script/main.js
+++ Sexcolor/script/main.js
@@ -1,3 +1,7 @@
-// 2.  Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+// Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
 // Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve essere azzurro
 // o rosa a seconda del sesso inserito.
+
+// https://it.wikipedia.org/wiki/Simon_(gioco)
+
+// IDEA: 
--- Simon/script/main.js
+++ Simon/script/main.js
@@ -1,4 +1,16 @@
 // Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi
 // Dopo 30 secondi l’utente deve inserire un prompt alla volta i numeri che ha
-// visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice 
+// visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice
 // quanti e quali dei numeri da indovinare sono stati individuati;
+
+// Un alert espone 5 numeri casuali.
+
+// creo la funzione
+function randnum (){
+  var num = [Math.floor((Math.random()*100)+1),Math.floor((Math.random()*100)+1), Math.floor((Math.random()*100)+1), Math.floor((Math.random()*100)+1), Math.floor((Math.random()*100)+1)];
+  alert(num);
+}
+// e la invoco
+randnum ();
+
+// Da li parte un timer di 30 secondi
